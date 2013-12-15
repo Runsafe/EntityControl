6d8aedc138a5dee273e56b7d171ecd295b7b7223
Morten Nilsen <morten@runsafe.no>
2013-12-15 12:37:08 +0100

    Declare features used

--- src/no/runsafe/entitycontrol/Plugin.java
+++ src/no/runsafe/entitycontrol/Plugin.java
@@ -1,12 +1,15 @@
 package no.runsafe.entitycontrol;
 
 import no.runsafe.framework.RunsafeConfigurablePlugin;
+import no.runsafe.framework.features.Events;
 
 public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		addComponent(Events.class);
+
 		this.addComponent(Options.class);
 		this.addComponent(EntityPortalCreation.class);
 		this.addComponent(EntityDeath.class);
