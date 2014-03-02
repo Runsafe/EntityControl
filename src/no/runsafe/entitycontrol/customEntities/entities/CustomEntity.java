c28d5259ff7b8cd423dedb4597ecfbdaa065c639
Kruithne <kruithne@gmail.com>
2014-03-02 04:27:04 +0000

    Remove unused import.

--- src/no/runsafe/entitycontrol/customEntities/entities/CustomEntity.java
+++ src/no/runsafe/entitycontrol/customEntities/entities/CustomEntity.java
@@ -1,6 +1,5 @@
 package no.runsafe.entitycontrol.customEntities.entities;
 
-import net.minecraft.server.v1_7_R1.EntityCreature;
 import net.minecraft.server.v1_7_R1.EntityPig;
 import net.minecraft.server.v1_7_R1.World;
 
