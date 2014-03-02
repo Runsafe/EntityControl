67ad68839bc27ba8c5ee519670e2a266e533c7a2
Kruithne <kruithne@gmail.com>
2014-03-02 07:38:36 +0000

    Persist our data in the NBT storage.

--- src/no/runsafe/entitycontrol/customEntities/CustomEntityData.java
+++ src/no/runsafe/entitycontrol/customEntities/CustomEntityData.java
@@ -4,13 +4,6 @@ import net.minecraft.server.v1_7_R1.*;
 import no.runsafe.entitycontrol.customEntities.entities.CustomEntity;
 import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
-import no.runsafe.framework.minecraft.Item;
-import no.runsafe.framework.minecraft.item.meta.RunsafeLeatherArmor;
-import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
-import no.runsafe.framework.minecraft.item.meta.RunsafeSkull;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
 
 public class CustomEntityData
 {
@@ -29,102 +22,12 @@ public class CustomEntityData
 			CustomEntity entity = type.getMobType().getConstructor(World.class).newInstance(world);
 			entity.setPosition(location.getX(), location.getY(), location.getZ());
 			world.addEntity(entity);
-			processData(entity);
+			entity.setData(data);
 			return entity;
 		}
 		catch (Exception e)
 		{
 			return null;
-		}
-	}
-
-	private void processData(CustomEntity entity)
-	{
-		HashMap<String, String> dataMap = new HashMap<String, String>(0);
-		String[] parts = data.split(",");
-		for (String part : parts)
-		{
-			String[] nodeSplit = part.split(":");
-			String value = nodeSplit.length > 1 ? nodeSplit[1] : null;
-			dataMap.put(nodeSplit[0], value);
-		}
-
-		if (dataMap.containsKey("name"))
-			entity.setCustomName(dataMap.get("name"));
-
-		if (dataMap.containsKey("root"))
-			entity.setCanMove(false);
-		else
-			entity.goalSelector().a(5, new PathfinderGoalRandomStroll(entity, 1.0D));
-
-		if (dataMap.containsKey("float"))
-			entity.goalSelector().a(1, new PathfinderGoalFloat(entity));
-
-		if (dataMap.containsKey("flee"))
-			entity.goalSelector().a(3, new PathfinderGoalFleeSun(entity, 1.0D));
-
-		if (dataMap.containsKey("watch"))
-			entity.goalSelector().a(6, new PathfinderGoalLookAtPlayer(entity, EntityHuman.class, 8.0F));
-
-		if (dataMap.containsKey("atk"))
-		{
-			entity.goalSelector().a(2, new PathfinderGoalMeleeAttack(entity, EntityHuman.class, 1.0D, false));
-			entity.targetSelector().a(2, new PathfinderGoalNearestAttackableTarget(entity, EntityHuman.class, 0, true));
-		}
-
-		if (dataMap.containsKey("god"))
-			entity.setInvincible(true);
-
-		if (dataMap.containsKey("fireproof"))
-		{
-			try
-			{
-				Field fireproof = Entity.class.getDeclaredField("fireProof");
-				fireproof.setAccessible(true);
-				fireproof.set(entity, true);
-			}
-			catch (Exception e)
-			{
-				// Oh noes!
-			}
-		}
-
-		if (dataMap.containsKey("hp"))
-			entity.setHealth(Float.valueOf(dataMap.get("hp")));
-
-		int colour = -1;
-
-		if (dataMap.containsKey("hex"))
-			colour = Integer.valueOf(dataMap.get("hex"), 16);
-
-		equip("hlm", 4, dataMap, entity, colour);
-		equip("wep", 0, dataMap, entity, colour);
-		equip("cst", 3, dataMap, entity, colour);
-		equip("leg", 2, dataMap, entity, colour);
-		equip("bts", 1, dataMap, entity, colour);
-
-		if (dataMap.containsKey("head"))
-		{
-			RunsafeSkull skull = (RunsafeSkull) Item.Decoration.Head.Human.getItem();
-			skull.setOwner(dataMap.get("head"));
-			entity.setEquipment(4, ObjectUnwrapper.getMinecraft(skull));
-		}
-	}
-
-	private void equip(String type, int slot, HashMap<String, String> data, CustomEntity entity, int colour)
-	{
-		if (data.containsKey(type))
-		{
-			Item object = Item.get(data.get(type));
-			if (object != null)
-			{
-				RunsafeMeta itemStack = object.getItem();
-
-				if (colour > -1 && itemStack instanceof RunsafeLeatherArmor)
-					((RunsafeLeatherArmor) itemStack).setColor(colour);
-
-				entity.setEquipment(slot, ObjectUnwrapper.getMinecraft(itemStack));
-			}
 		}
 	}
 
--- src/no/runsafe/entitycontrol/customEntities/entities/CustomEntity.java
+++ src/no/runsafe/entitycontrol/customEntities/entities/CustomEntity.java
@@ -1,9 +1,14 @@
 package no.runsafe.entitycontrol.customEntities.entities;
 
 import net.minecraft.server.v1_7_R1.*;
+import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
+import no.runsafe.framework.minecraft.item.meta.RunsafeLeatherArmor;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
+import no.runsafe.framework.minecraft.item.meta.RunsafeSkull;
 import org.bukkit.craftbukkit.v1_7_R1.util.UnsafeList;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
 
 public abstract class CustomEntity extends EntitySkeleton
 {
@@ -95,16 +100,115 @@ public abstract class CustomEntity extends EntitySkeleton
 		return groupdataentity;
 	}
 
-	public PathfinderGoalSelector goalSelector()
+	@Override
+	public void a(NBTTagCompound nbttagcompound)
 	{
-		return goalSelector;
+		super.a(nbttagcompound);
+		if (nbttagcompound.hasKey("customData"))
+			setData(nbttagcompound.getString("customData"));
 	}
 
-	public PathfinderGoalSelector targetSelector()
+	@Override
+	public void b(NBTTagCompound nbttagcompound)
 	{
-		return targetSelector;
+		super.b(nbttagcompound);
+		nbttagcompound.setString("customData", data);
+	}
+
+	public void setData(String data)
+	{
+		this.data = data;
+		processData(data);
+	}
+
+	private void processData(String data)
+	{
+		HashMap<String, String> dataMap = new HashMap<String, String>(0);
+		String[] parts = data.split(",");
+		for (String part : parts)
+		{
+			String[] nodeSplit = part.split(":");
+			String value = nodeSplit.length > 1 ? nodeSplit[1] : null;
+			dataMap.put(nodeSplit[0], value);
+		}
+
+		if (dataMap.containsKey("name"))
+			setCustomName(dataMap.get("name"));
+
+		if (dataMap.containsKey("root"))
+			setCanMove(false);
+		else
+			goalSelector.a(5, new PathfinderGoalRandomStroll(this, 1.0D));
+
+		if (dataMap.containsKey("float"))
+			goalSelector.a(1, new PathfinderGoalFloat(this));
+
+		if (dataMap.containsKey("flee"))
+			goalSelector.a(3, new PathfinderGoalFleeSun(this, 1.0D));
+
+		if (dataMap.containsKey("watch"))
+			goalSelector.a(6, new PathfinderGoalLookAtPlayer(this, EntityHuman.class, 8.0F));
+
+		if (dataMap.containsKey("atk"))
+		{
+			goalSelector.a(2, new PathfinderGoalMeleeAttack(this, EntityHuman.class, 1.0D, false));
+			targetSelector.a(2, new PathfinderGoalNearestAttackableTarget(this, EntityHuman.class, 0, true));
+		}
+
+		if (dataMap.containsKey("god"))
+			setInvincible(true);
+
+		if (dataMap.containsKey("fireproof"))
+		{
+			try
+			{
+				Field fireproof = Entity.class.getDeclaredField("fireProof");
+				fireproof.setAccessible(true);
+				fireproof.set(this, true);
+			}
+			catch (Exception e)
+			{
+				// Oh noes!
+			}
+		}
+
+		int colour = -1;
+
+		if (dataMap.containsKey("hex"))
+			colour = Integer.valueOf(dataMap.get("hex"), 16);
+
+		equip("hlm", 4, dataMap, colour);
+		equip("wep", 0, dataMap, colour);
+		equip("cst", 3, dataMap, colour);
+		equip("leg", 2, dataMap, colour);
+		equip("bts", 1, dataMap, colour);
+
+		if (dataMap.containsKey("head"))
+		{
+			RunsafeSkull skull = (RunsafeSkull) no.runsafe.framework.minecraft.Item.Decoration.Head.Human.getItem();
+			skull.setOwner(dataMap.get("head"));
+			setEquipment(4, ObjectUnwrapper.getMinecraft(skull));
+		}
+	}
+
+	private void equip(String type, int slot, HashMap<String, String> data, int colour)
+	{
+		if (data.containsKey(type))
+		{
+			no.runsafe.framework.minecraft.Item object = no.runsafe.framework.minecraft.Item.get(data.get(type));
+			if (object != null)
+			{
+				RunsafeMeta itemStack = object.getItem();
+
+				if (colour > -1 && itemStack instanceof RunsafeLeatherArmor)
+					((RunsafeLeatherArmor) itemStack).setColor(colour);
+
+				setEquipment(slot, ObjectUnwrapper.getMinecraft(itemStack));
+			}
+		}
 	}
 
 	private boolean canMove = true;
 	private boolean invincible = false;
+	private String data;
 }
