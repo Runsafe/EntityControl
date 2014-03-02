9e6525b09912dc4eedecce26f1197b0ac861b634
Kruithne <kruithne@gmail.com>
2014-03-02 04:01:20 +0000

    Extend pig AI for testing purposes.

--- src/no/runsafe/entitycontrol/customEntities/entities/CustomEntity.java
+++ src/no/runsafe/entitycontrol/customEntities/entities/CustomEntity.java
@@ -1,9 +1,10 @@
 package no.runsafe.entitycontrol.customEntities.entities;
 
 import net.minecraft.server.v1_7_R1.EntityCreature;
+import net.minecraft.server.v1_7_R1.EntityPig;
 import net.minecraft.server.v1_7_R1.World;
 
-public abstract class CustomEntity extends EntityCreature
+public abstract class CustomEntity extends EntityPig
 {
 	public CustomEntity(World world)
 	{
