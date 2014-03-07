14d92e28e02efa1f3f62b6ec496d92b7e7a05a02
Kruithne <kruithne@gmail.com>
2014-03-07 08:48:41 +0000

    Disregard mount type for now.

--- src/no/runsafe/entitycontrol/MountedTeleportHandler.java
+++ src/no/runsafe/entitycontrol/MountedTeleportHandler.java
@@ -19,7 +19,7 @@ public class MountedTeleportHandler implements IPlayerTeleportEvent
 		if (player.isInsideVehicle() && to != null && from != null && to.getWorld().isWorld(from.getWorld()) && to.distance(from) > 500)
 		{
 			IEntity vehicle = player.getVehicle();
-			if (vehicle != null && vehicle.getEntityType() == LivingEntity.Horse)
+			if (vehicle != null)
 			{
 				vehicle.eject();
 				vehicle.teleport(to);
