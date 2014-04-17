7d44f68a7da0109907031dd05897c66f5e18b3f8
Kruithne <kruithne@gmail.com>
2014-04-17 16:53:00 +0100

    Remove all default path-finders from the companion entity.

--- src/no/runsafe/entitycontrol/pets/CompanionPet.java
+++ src/no/runsafe/entitycontrol/pets/CompanionPet.java
@@ -1,12 +1,29 @@
 package no.runsafe.entitycontrol.pets;
 
 import net.minecraft.server.v1_7_R2.EntityZombie;
+import net.minecraft.server.v1_7_R2.PathfinderGoalSelector;
 import net.minecraft.server.v1_7_R2.World;
+import org.bukkit.craftbukkit.v1_7_R2.util.UnsafeList;
+
+import java.lang.reflect.Field;
 
 public class CompanionPet extends EntityZombie
 {
 	public CompanionPet(World world)
 	{
 		super(world);
+
+		// Remove all default path-finders.
+		try
+		{
+			Field gsa = PathfinderGoalSelector.class.getDeclaredField("b");
+			gsa.setAccessible(true);
+			gsa.set(this.goalSelector, new UnsafeList());
+			gsa.set(this.targetSelector, new UnsafeList());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
