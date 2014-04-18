3637ddefb5c1f86dc016befbb0b8aa2ecb01f112
Kruithne <kruithne@gmail.com>
2014-04-18 03:19:44 +0100

    Villagers are animals.. it seems.

--- src/no/runsafe/entitycontrol/pets/VillagerCompanion.java
+++ src/no/runsafe/entitycontrol/pets/VillagerCompanion.java
@@ -3,7 +3,7 @@ package no.runsafe.entitycontrol.pets;
 import net.minecraft.server.v1_7_R2.EntityHuman;
 import net.minecraft.server.v1_7_R2.World;
 
-public class VillagerCompanion extends CompanionPetHumanoid
+public class VillagerCompanion extends CompanionPetAnimal
 {
 	public VillagerCompanion(World world)
 	{
