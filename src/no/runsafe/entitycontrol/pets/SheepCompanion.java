5b99787575abfc70ba723fcff64248df6a881cf8
Kruithne <kruithne@gmail.com>
2014-04-22 16:43:50 +0100

    Make the sheep companion randomly change colour every 10 minutes.

--- src/no/runsafe/entitycontrol/pets/SheepCompanion.java
+++ src/no/runsafe/entitycontrol/pets/SheepCompanion.java
@@ -3,6 +3,8 @@ package no.runsafe.entitycontrol.pets;
 import net.minecraft.server.v1_7_R2.EntityHuman;
 import net.minecraft.server.v1_7_R2.World;
 
+import java.util.Random;
+
 public class SheepCompanion extends CompanionPetAnimal
 {
 	public SheepCompanion(World world)
@@ -16,4 +18,20 @@ public class SheepCompanion extends CompanionPetAnimal
 		playSound("mob.sheep.say");
 		return false;
 	}
+
+	@Override
+	public void B()
+	{
+		super.B(); // Entiy base tick
+
+		if (colourChangeTicks > 0)
+		{
+			colourChangeTicks = 12000;
+			datawatcher.watch(16, datawatcher.getByte(16) & 240 | (random.nextInt(15) + 1) & 15);
+		}
+		colourChangeTicks--;
+	}
+
+	private int colourChangeTicks = 12000;
+	private final Random random = new Random();
 }
