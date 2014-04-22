72d1d4da06cda1ec6d9563b3999814681afc436e
Kruithne <kruithne@gmail.com>
2014-04-22 17:45:47 +0100

    No longer need to use a secondary command class here as we've removed the "/npc" command.

--- src/no/runsafe/entitycontrol/Plugin.java
+++ src/no/runsafe/entitycontrol/Plugin.java
@@ -30,7 +30,7 @@ public class Plugin extends RunsafeConfigurablePlugin
 		// Companions
 		addComponent(CompanionHandler.class);
 
-		Command companionCommand = new SecondaryCommand("companion", "Companion related commands", null);
+		Command companionCommand = new Command("companion", "Companion related commands", null);
 		addComponent(companionCommand);
 		companionCommand.addSubCommand(getInstance(SpawnCompanion.class));
 		companionCommand.addSubCommand(getInstance(CreateEgg.class));
