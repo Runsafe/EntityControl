272c7b2b5f790d74db8c7c2f1f74d6f7fed38cb5
Kruithne <kruithne@gmail.com>
2014-04-22 16:58:31 +0100

    Possible fix: Client crash when sheep companion changes colour.

--- src/no/runsafe/entitycontrol/pets/SheepCompanion.java
+++ src/no/runsafe/entitycontrol/pets/SheepCompanion.java
@@ -27,7 +27,8 @@ public class SheepCompanion extends CompanionPetAnimal
 		if (colourChangeTicks == 0)
 		{
 			colourChangeTicks = 12000;
-			datawatcher.watch(16, datawatcher.getByte(16) & 240 | (random.nextInt(15) + 1) & 15);
+			int randomColour = colours[random.nextInt(colours.length)];
+			datawatcher.watch(16, datawatcher.getByte(16) & 240 | randomColour & 15);
 		}
 		colourChangeTicks--;
 	}
@@ -35,4 +36,5 @@ public class SheepCompanion extends CompanionPetAnimal
 	//private int colourChangeTicks = 12000;
 	private int colourChangeTicks = 1000;
 	private final Random random = new Random();
+	private int[] colours = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
 }
